95d88addb5eec2b1ad9ac069b16983dbf04096f3
mohamed mouine <med-ingeno@Sansfil-Eduroam-Lassonde-212-166.polymtl.ca>
2019-03-23 16:56:41 -0400

    add tests

--- src/application/account/utils/TokenUtil.ts
+++ src/application/account/utils/TokenUtil.ts
@@ -3,17 +3,17 @@ import * as config from 'config';
 
 export class TokenUtil {
   private static EXPIRES_IN = '1h';
-
+  private static JWT_SECRET = '44a0a45f31cf8122651e28710a43530e'
   public static createNewToken(userId: string): string {
-    return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: TokenUtil.EXPIRES_IN });
+    return jwt.sign({ id: userId }, TokenUtil.JWT_SECRET, { expiresIn: TokenUtil.EXPIRES_IN });
   }
 
   public static isValidToken (token: string): boolean {
-    return !!jwt.verify(token, process.env.JWT_SECRET);
+    return !!jwt.verify(token, TokenUtil.JWT_SECRET);
   }
 
   public static doesOwnToken (userId: string, token: string): boolean {
-    const id: string | object = jwt.verify(token, process.env.JWT_SECRET);
+    const id: string | object = jwt.verify(token, TokenUtil.JWT_SECRET);
     return id === userId;
   }
 }
