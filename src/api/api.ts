7e739a895a1d1a5c82f59d1a832da03617af8fec
mohamed mouine <med-ingeno@Sansfil-Eduroam-Lassonde-212-166.polymtl.ca>
2019-03-23 10:23:19 -0400

    add base setup

--- src/api/api.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-export class Api {
-    public getRouter(): Router {
-        const router = Router();
-        // TODO: You probably want to register your routes here.
-        return router;
-    }
-}
---
+++ src/binding.ts
@@ -0,0 +1,5 @@
+import { Container } from 'inversify';
+import types from './context/types';
+
+export default function (container: Container): void {
+}
